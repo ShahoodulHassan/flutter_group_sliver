52e303384f8e5925f45f09e5501e905ebd125fd6
Shahood <shahood@gmail.com>
2023-01-26 02:13:57 +0500

    Fixed deprecation issue of .insertChildRenderObject()

--- lib/src/widget/sliver_group.dart
+++ lib/src/widget/sliver_group.dart
@@ -70,21 +70,25 @@ class _SliverGroupElement extends RenderObjectElement {
   }
 
   @override
-  void insertChildRenderObject(RenderObject child, int? slot) {
-    final _RenderSliverGroup renderObject = this.renderObject as _RenderSliverGroup;
+  void insertRenderObjectChild(RenderObject child, int? slot) {
+    final _RenderSliverGroup renderObject =
+        this.renderObject as _RenderSliverGroup;
     if (slot == 0) renderObject.decoration = child as RenderBox?;
     if (slot == 1) renderObject.child = child as RenderSliver?;
     assert(renderObject == this.renderObject);
   }
 
   @override
-  void moveChildRenderObject(RenderObject child, slot) {
+  void moveRenderObjectChild(covariant RenderObject child,
+      covariant Object? oldSlot, covariant Object? newSlot) {
     assert(false);
   }
 
   @override
-  void removeChildRenderObject(RenderObject child) {
-    final _RenderSliverGroup renderObject = this.renderObject as _RenderSliverGroup;
+  void removeRenderObjectChild(
+      covariant RenderObject child, covariant Object? slot) {
+    final _RenderSliverGroup renderObject =
+        this.renderObject as _RenderSliverGroup;
     if (renderObject.decoration == child) renderObject.decoration = null;
     if (renderObject.child == child) renderObject.child = null;
     assert(renderObject == this.renderObject);
